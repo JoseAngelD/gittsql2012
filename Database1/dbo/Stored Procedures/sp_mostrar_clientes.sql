abed269cf8ff4d3070066cab035e9da85ae68396
JoseAngelD <jadiazbecerra@gmail.com>
2022-01-08 13:49:27 -0500

    Cambios Store Procedures

--- Database1/dbo/Stored Procedures/sp_mostrar_clientes.sql
+++ Database1/dbo/Stored Procedures/sp_mostrar_clientes.sql
@@ -1,8 +1,9 @@
-﻿create proc sp_mostrar_clientes (@num_cliente varchar(20))
+﻿CREATE proc [dbo].[sp_mostrar_clientes] (@num_cliente varchar(20))
 as
 begin
 
 select * from sales.Customers
 where custid=@num_cliente
 
+--create un nuevo cambio
 end
--- Database1/dbo/Stored Procedures/sp_mostrar_productos.sql
+++ Database1/dbo/Stored Procedures/sp_mostrar_productos.sql
@@ -1,9 +1,11 @@
 ﻿
 
-create proc sp_mostrar_productos 
+CREATE proc [dbo].[sp_mostrar_productos] 
 as
 begin
 select c.categoryname,p.productname from Production.Products p 
 inner join Production.Categories c
 on p.categoryid=p.categoryid
+
+-- otro cambio
 end
